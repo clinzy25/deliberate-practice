2517a0cf80318b0e2d2f290188f85a1cc44d3ee1
clinzy25 <conner.linzy25@gmail.com>
2021-06-07 14:29:41 -0400

    store reducers types setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './index.css';
 import './App.css';
+import { EntryContainer } from './components/EntryContainer';
 
-export const App: React.FC = (): JSX.Element => {
-  
-
+export const App: React.FC = () => {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='app-container flex flex-col place-items-center'>
+      <header>
+        <h1 className='font-bold text-4xl'>Deliberate Practice</h1>
+      </header>
+      <EntryContainer />
     </div>
   );
 };
